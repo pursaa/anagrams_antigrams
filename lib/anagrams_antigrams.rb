491cb8ada8c17bf70b55b99fec0cbb9fcb51898a
Guest User <Guest@epicodus-27.local>
2017-09-08 15:58:03 -0700

    It will compare two words and see if they are equal

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,13 +1,13 @@
 
-class Letters
-  attr_reader(:word1, :word2)
+# class Letters
+#   attr_reader(:word1, :word2)
 
 
-  def initialize(word1)
-    @word1 = word1
-    @word2 = word2
-  end
-end
+#   def initialize(word1)
+#     @word1 = word1
+#     @word2 = word2
+#   end
+# end
 
 public def match(word1, word2)
   if(word1 == word2)
@@ -21,8 +21,9 @@ end
     word1Input = gets.chomp
     puts "Enter word 2"
     word2Input = gets.chomp
-    firstWord = Letters.new(word1Input)
-    secondWord = Letters.new(word2Input)
+    # firstWord = Letters.new(word1Input)
+    # secondWord = Letters.new(word2Input)
 
-    result = match(firstWord, secondWord)
+    # result = match(firstWord, secondWord)
+    result = match(word1Input, word2Input)
 puts result
